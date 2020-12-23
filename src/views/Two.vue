421c6a52f35aeefeb7f99700e46d3cf425401d23
10ku <keprosj@gmail.com>
2020-12-22 22:13:14 -0600

    Replace About.vue

---
+++ src/views/Two.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Two">
+    <h1>2</h1>
+    <MyComponent/>
+  </div>
+</template>
+
+<script>
+import MyComponent from '@/components/MyComponent.vue'
+
+export default {
+  name: 'Root',
+  components: {
+    MyComponent
+  }
+}
+</script>
