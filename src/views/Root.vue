48d92e53a6960aaab4f772ff0c308b81b6a3e417
10ku <keprosj@gmail.com>
2020-12-22 22:12:58 -0600

    Replace Home.vue

---
+++ src/views/Root.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Root">
+    <MyComponent/>
+  </div>
+</template>
+
+<script>
+import MyComponent from '@/components/MyComponent.vue'
+
+export default {
+  name: 'Root',
+  components: {
+    MyComponent
+  }
+}
+</script>
