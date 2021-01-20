b3bfa027d19fc77c7778545d00c71e015aeec1df
10ku <keprosj@gmail.com>
2021-01-19 23:13:39 -0600

    Add modified nav bar for login page

---
+++ Client/src/components/NavigateLogin.vue
@@ -0,0 +1,50 @@
+<template>
+<div id="navigate">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<div class="flex-container">
+		<div class="bar-left">
+			<router-link to="/" class="flex-item material-icons">home</router-link>
+		</div>
+	</div>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Navigate extends Vue
+{
+	// @Prop() private msg!: string;
+}
+</script>
+
+<style scoped>
+div.flex-container
+{
+	display: flex;
+	color: white;
+	background: rgb(69, 116, 187);
+	border-bottom: 1px solid rgb(0, 99, 168);
+	height: 48px;
+	padding: 0 16px;
+	align-items: center;
+	justify-content: space-between;
+}
+button
+{
+	border: none;
+	color: white;
+	background: transparent;
+	cursor: pointer;
+}
+.bar-center
+{
+	flex: 0 1 664px;
+}
+.search-bar
+{
+	width: 80%;
+	font-size: 18px;
+}
+</style>
