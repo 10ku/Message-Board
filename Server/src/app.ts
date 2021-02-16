3f09bd23da631f3e7f1c3ca3f6f73887595c2038
10ku <keprosj@gmail.com>
2021-02-16 00:30:30 -0600

    Use mongoose to connect to db

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,39 +1,24 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import MongoClient from "mongodb";
-import assert from "assert";
+import mongoose from "mongoose";
 
 const app = express();
+const port = 3000;
+const url = "mongodb://localhost:27017/web_example_db";
+
 app.use(express.json());
 app.use(cors());
 app.use(morgan("combined"));
 
-const port = 3000;
-const url = 'mongodb://localhost:27017';
-const dbName = 'web_example_db';
-
-MongoClient.connect(url, function(err, client)
-{
-	assert.strictEqual(null, err);
-	console.log("Connected successfully to server");
-
-	const db = client.db(dbName);
-	const collection = db.collection('web_example_collection');
-	var data = collection.find({}).toArray(function(err, docs) {
-	assert.strictEqual(err, null);
-	console.log("Found the following records");
-	app.get('/', (req, res) => {
-		res.json(docs)
-	})
-	console.log(docs)
-	});
-	client.close();
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once("open", function() {
+	console.log("Connection Successful");
 });
-
-
 
 app.listen(port, () =>
 {
 	console.log(`Example app listening at http://localhost:${port}`)
-})
+});
