af0bc00bd3b58ed33056f911b09f72421c1c9d8d
10ku <keprosj@gmail.com>
2021-02-16 00:31:25 -0600

    Link /login to userRoute

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import mongoose from "mongoose";
+import userRoute from "./routes/api/userRoute";
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,7 @@ const url = "mongodb://localhost:27017/web_example_db";
 app.use(express.json());
 app.use(cors());
 app.use(morgan("combined"));
+app.use("/login", userRoute);
 
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
