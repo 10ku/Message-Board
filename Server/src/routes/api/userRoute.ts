46cd373649c941777ef7046495412dde898e8e83
10ku <keprosj@gmail.com>
2021-02-15 23:57:59 -0600

    Add basic user account route

---
+++ Server/src/routes/api/userRoute.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import { userModel } from "../../models/userModel";
+
+const user = Router();
+
+user.get("/", async (req, res) => {
+	try {
+		const users = await userModel.find()
+		if (!users) throw new Error("No Users!")
+		const sorted = users.sort()
+		res.status(200).json(sorted)	
+	} catch (error) {
+		res.status(500).json({message: error.message})
+	}	
+});
+
+user.post("/", async (req, res) => {
+	const newUser = new userModel(req.body)
+	try {
+		const userDocument = await newUser.save()
+		if (!userDocument) throw new Error("Bad Save!")
+		res.status(200).json(userDocument)	
+	} catch (error) {
+		res.status(500).json({message: error.message})
+	}	
+});
+
+export default user;
