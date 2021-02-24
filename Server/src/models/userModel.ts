1fc305c62acb37bebce469d750dd37d4537cf9d7
10ku <keprosj@gmail.com>
2021-02-23 20:19:36 -0600

    Add validation to schema

--- Server/src/models/userModel.ts
+++ Server/src/models/userModel.ts
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-	username:   String,
-	// email:  String,
-	password: String,
-	// date: { type: Date, default: Date.now }
+	email: { type: String, trim: true, match: /[A-z0-9]+@[A-z0-9]+.[A-z0-9]+/, maxLength: 80, unique: true, required: true },
+	username: { type: String, trim: true, maxLength: 80, unique: true, required: true },
+	password: { type: String, match: /[^\s]/g, minLength: 8, maxLength: 255, required: true },
+	date_of_creation: { type: Date, default: Date.now, required: true }
 });
 
 export const userModel = mongoose.model("", userSchema, "web_example_collection");
