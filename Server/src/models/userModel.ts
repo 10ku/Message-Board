591595f63e82b0872e4b9a8329529041f3a18d3d
10ku <keprosj@gmail.com>
2021-03-09 18:02:08 -0600

    Hash password when saving to database

--- Server/src/models/userModel.ts
+++ Server/src/models/userModel.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
 
 const { Schema } = mongoose;
 
@@ -7,6 +8,23 @@ const userSchema = new Schema({
 	username: { type: String, trim: true, maxLength: 80, unique: true, required: true },
 	password: { type: String, match: /[^\s]/g, minLength: 8, maxLength: 255, required: true },
 	date_of_creation: { type: Date, default: Date.now, required: true }
+});
+
+userSchema.pre("save", async function save(next)
+{
+	if (!this.isModified("password")) return next();
+
+	try
+	{
+	  const salt = await bcryptjs.genSalt(8);
+	  //@ts-ignore
+	  this.password = await bcryptjs.hash(this.password, salt);
+	  return next();
+	}
+	catch (err)
+	{
+	  return next(err);
+	}
 });
 
 export const userModel = mongoose.model("", userSchema, "web_example_collection");
