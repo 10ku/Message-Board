e8b296825becd9a1813fcda1e656221e45beacdb
10ku <keprosj@gmail.com>
2021-03-18 00:01:27 -0500

    Include vuetify

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue"
 import router from "./router"
 import { sync } from "vuex-router-sync"
 import store from "@/store/store"
+import vuetify from "@/plugins/vuetify"
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,6 @@ sync(store, router)
 new Vue({
 	router,
 	store,
+	vuetify,
 	render: h => h(App)
 }).$mount("#app")
