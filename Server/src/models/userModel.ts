239bb8dfe0302447e6a70a335605818af96edad7
10ku <keprosj@gmail.com>
2021-03-18 00:00:19 -0500

    Improve regex

--- Server/src/models/userModel.ts
+++ Server/src/models/userModel.ts
@@ -15,9 +15,9 @@ export interface userSchemaInterface extends Document
 
 const userSchema = new Schema(
 {
-	email: { type: String, trim: true, match: /[A-z0-9]+@[A-z0-9]+.[A-z0-9]+/, maxLength: 80, unique: true, required: true },
+	email: { type: String, trim: true, match: /^[A-z0-9]+@[A-z0-9]+\.[A-z0-9]+$/, maxLength: 80, unique: true, required: true },
 	username: { type: String, trim: true, maxLength: 80, unique: true, required: true },
-	password: { type: String, match: /[^\s]/g, minLength: 8, maxLength: 255, required: true },
+	password: { type: String, match: /^[A-z0-9]+$/, minLength: 8, maxLength: 255, required: true },
 	date_of_creation: { type: Date, default: Date.now, required: true }
 });
 
