a773391120335c68979da4a0906fd7f477634c01
10ku <keprosj@gmail.com>
2021-03-18 00:20:18 -0500

    Remove login navbar component

--- Client/src/views/LoginRegister.vue
+++ Client/src/views/LoginRegister.vue
@@ -1,19 +1,16 @@
 <template>
 	<div class="LoginRegister">
-		<NavigateLogin/>
 		<LoginCreateAccount/>
 	</div>
 </template>
 
 <script>
 import LoginCreateAccount from "@/components/LoginCreateAccount.vue"
-import NavigateLogin from "@/components/NavigateLogin.vue"
 
 export default {
 	name: "LoginRegister",
 	components: {
-		LoginCreateAccount,
-		NavigateLogin
+		LoginCreateAccount
 	}
 }
 </script>
