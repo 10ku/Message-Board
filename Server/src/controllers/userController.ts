f372eebbc763f93a641e2e9f45398ffcfe195fe7
10ku <keprosj@gmail.com>
2021-04-08 18:51:08 -0500

    Move logic into controller class

---
+++ Server/src/controllers/userController.ts
@@ -0,0 +1,65 @@
+import appConfig from "../config/config"
+import { userModel } from "../models/userModel";
+import jwt from "jsonwebtoken";
+
+export default class UserController
+{
+	private static jwtSignUser(user: object)
+	{
+		return jwt.sign({user}, appConfig.settings.jwt.jwtSecret,
+		{
+			expiresIn: appConfig.settings.jwt.jwtExpiration
+		})
+	}
+
+	static async register(req: any, res: any)
+	{
+		const newUser = new userModel(req.body)
+
+		try
+		{
+			const userDocument = await newUser.save()
+
+			if (!userDocument) throw new Error("Bad Save!")
+
+			res.status(200).json(
+			{
+				userDocument,
+				token: UserController.jwtSignUser(userDocument)
+			})
+		}
+		catch (error)
+		{
+			res.status(500).json({message: error.message})
+		}	
+	}
+
+	static async login(req: any, res: any)
+	{
+		const {email, password} = req.body
+
+		try
+		{
+			const userDocument = await userModel.findOne(
+			{
+				email: email,
+			})
+
+			if (!userDocument) throw new Error("Bad Credentials")
+
+			const result = await userDocument.validatePassword(password)
+
+			if (!result) throw new Error("Bad Credentials")
+
+			res.status(200).json(
+			{
+				userDocument,
+				token: UserController.jwtSignUser(userDocument)
+			})
+		}
+		catch (error)
+		{
+			res.status(500).json({message: error.message})
+		}	
+	}
+}
--- Server/src/routes/userRoute.ts
+++ Server/src/routes/userRoute.ts
@@ -1,67 +1,9 @@
-import appConfig from "../config/config"
 import { Router } from "express";
-import { userModel } from "../models/userModel";
-import jwt from "jsonwebtoken";
+import userController from "../controllers/userController";
 
 const userRoute = Router();
 
-function jwtSignUser(user: object)
-{
-	return jwt.sign({user}, appConfig.settings.jwt.jwtSecret,
-	{
-		expiresIn: appConfig.settings.jwt.jwtExpiration
-	})
-}
-
-userRoute.post("/register", async (req, res) =>
-{
-	const newUser = new userModel(req.body)
-
-	try
-	{
-		const userDocument = await newUser.save()
-
-		if (!userDocument) throw new Error("Bad Save!")
-
-		res.status(200).json(
-		{
-			userDocument,
-			token: jwtSignUser(userDocument)
-		})
-	}
-	catch (error)
-	{
-		res.status(500).json({message: error.message})
-	}	
-});
-
-userRoute.post("/login", async (req, res) =>
-{
-	const {email, password} = req.body
-
-	try
-	{
-		const userDocument = await userModel.findOne(
-		{
-			email: email,
-		})
-
-		if (!userDocument) throw new Error("Bad Credentials")
-
-		const result = await userDocument.validatePassword(password)
-
-		if (!result) throw new Error("Bad Credentials")
-
-		res.status(200).json(
-		{
-			userDocument,
-			token: jwtSignUser(userDocument)
-		})
-	}
-	catch (error)
-	{
-		res.status(500).json({message: error.message})
-	}	
-});
+userRoute.post("/register", userController.register);
+userRoute.post("/login", userController.login);
 
 export default userRoute;
