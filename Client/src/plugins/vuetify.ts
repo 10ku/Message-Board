f071275dd25a793e648975b7ca74a342bc656864
10ku <keprosj@gmail.com>
2021-04-08 21:48:41 -0500

    Use dark Vuetify theme

--- Client/src/plugins/vuetify.ts
+++ Client/src/plugins/vuetify.ts
@@ -1,9 +1,28 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import Vue from "vue"
+import Vuetify from "vuetify"
+import "vuetify/dist/vuetify.min.css"
 
 Vue.use(Vuetify)
 
-const opts = {}
+const opts =
+{
+	theme:
+	{
+		dark: true,
+		themes:
+		{
+			dark:
+			{
+				primary: "#4067b5",
+				secondary: "#424242",
+				accent: "#5768c7",
+				error: "#b54040",
+				info: "#347cbc",
+				success: "#46914c",
+				warning: "#b57340"
+			}
+		}
+	}
+}
 
 export default new Vuetify(opts)
