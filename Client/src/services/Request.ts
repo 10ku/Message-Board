e3764eeae9ee161210ab65f43242c8d8e2f23e04
10ku <keprosj@gmail.com>
2021-04-12 21:39:39 -0500

    Add getPosts and makePost to Request class

--- Client/src/services/Request.ts
+++ Client/src/services/Request.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const myAxios = axios.create({
-	baseURL: "http://localhost:3000"
-});
+const myAxios = axios.create(
+	{
+		baseURL: "http://localhost:3000"
+	});
 
 export default class Request
 {
@@ -14,5 +15,15 @@ export default class Request
 	static login(credentials: object)
 	{
 		return myAxios.post("login", credentials);
+	}
+
+	static getPosts()
+	{
+		return myAxios.get("posts");
+	}
+
+	static makePost(post: object)
+	{
+		return myAxios.post("posts", post);
 	}
 }
